59f9939a1b4cc1edcaee3a6067ae50cadd65d55a
takuma1192 <78262525+takuma1192@users.noreply.github.com>
2021-04-05 21:40:44 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <id class="app">
+    <input type="text" maxlength="7" v-model="postcode"/>
+    <button @click="searchAddress">住所自動入力</button>
+      <p>Adderss：{{allAddress}}</p>
+  </id>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      postcode:"",
+      allAddress: ""
+    }
+  },
+  methods: {
+    searchAddress() {
+      this.axios.get(`https://apis.postcode-jp.com/api/v3/postcodes?postcode=w8HPtAhnk3waN3B52CFYOXSLzvOYKrWha0b8DCf`)
+      .then((response) =>{
+        this.allAddress = response.data.allAddress;
+      });
+    },
+  },
+};
+</script>
